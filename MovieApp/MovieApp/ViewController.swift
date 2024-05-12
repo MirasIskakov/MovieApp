8dc46fd1854ae7484b98ab8764c9778955dc29cf
Miras <iskakov91633@gmail.com>
2024-05-12 18:11:44 +0500

    setup projects

---
+++ MovieApp/MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Miras Iskakov on 12.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemCyan
+    }
+
+
+}
+
